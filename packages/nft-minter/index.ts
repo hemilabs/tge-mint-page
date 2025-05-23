881bad5772898df57965dc7f5773d75adfde699f
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-05-23 12:38:30 -0300

    Read abi from nftMinter package

---
+++ packages/nft-minter/index.ts
@@ -0,0 +1,2 @@
+export { erc721Abi } from "./src/abi";
+export { mintNFT } from "./src/mint";
--- website/src/hooks/nft.ts
+++ website/src/hooks/nft.ts
@@ -1,26 +1,11 @@
+import { erc721Abi } from "nft-minter";
 import { Address } from "viem";
 import { hemi } from "viem/chains";
 import { useReadContract } from "wagmi";
 
-const abi = [
-  {
-    inputs: [],
-    name: "imageURI",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-] as const;
-
 export const useImageURI = () =>
   useReadContract({
-    abi,
+    abi: erc721Abi,
     address: import.meta.env.VITE_NFT_CONTRACT_ADDRESS as Address,
     chainId: hemi.id,
     functionName: "imageURI",
