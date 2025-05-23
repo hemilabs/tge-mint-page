497ff9008ea943522e9473eb7530b75cddfe95ff
Gonzalo D'elia <gonzalo@hemi.xyz>
2025-05-23 12:46:47 -0300

    Unify nft address

--- website/src/hooks/nft.ts
+++ website/src/hooks/nft.ts
@@ -1,12 +1,11 @@
-import { erc721Abi } from "nft-minter";
-import { Address } from "viem";
+import { erc721Abi, nftAddress } from "nft-minter";
 import { hemi } from "viem/chains";
 import { useReadContract } from "wagmi";
 
 export const useImageURI = () =>
   useReadContract({
     abi: erc721Abi,
-    address: import.meta.env.VITE_NFT_CONTRACT_ADDRESS as Address,
+    address: nftAddress,
     chainId: hemi.id,
     functionName: "imageURI",
   });
